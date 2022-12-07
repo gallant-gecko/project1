d20bd0c98e9c041cc0ba755947d8d1730cb1d3b3
Patrick Haynes <phaynes@unomaha.edu>
2022-12-06 19:34:04 -0600

    add exception handling to controller

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from view import *
 import csv
+import os.path
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
@@ -13,69 +14,33 @@ class Controller(QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.hide_error_multiple()
-        self.hide_error_single()
         self.hide_success_multiple()
-        self.hide_success_single()
-        self.button_submit_multiple.clicked.connect(self.submit_multiple)
-        self.button_clear_multiple.clicked.connect(self.clear_multiple_inputs)
-        self.button_submit_single.clicked.connect(self.submit_single)
-        self.button_clear_single.clicked.connect(self.clear_single_inputs)
+        self.button_submit.clicked.connect(self.submit)
+        self.button_clear.clicked.connect(self.clear_multiple_inputs)
 
-    def submit_multiple(self):
+    def submit(self):
         """
         Function to submit input data found in multiple student calculation
         """
         self.hide_error_multiple()
         self.hide_success_multiple()
-        self.calculate_multiple()
-    
-    def submit_single(self):
-        """
-        Function to submit input data found in single student calculation
-        """
-        self.hide_error_single()
-        self.hide_success_single()
-        self.calculate_single()
+        try:
+            file = open(f'{self.input_csv_path.text()}', 'r')
+        except FileNotFoundError:
+            self.label_warning_multiple_1.setHidden(False)
+        else:
+            try:
+                highest_grade, data = self.parse_csv(file)
+            except ValueError:
+                self.label_warning_multiple_2.setHidden(False)
+            else:
+                self.output_csv(highest_grade, data)
 
-    def calculate_single(self):
-        """
-        Function to calculate single student curved grade and display to GUI
-        """
-        try:
-            highest_grade = int(self.input_highest_grade.text())
-            student_grade = int(self.input_student_grade.text())
-        except:
-            self.display_error_single()
-        else:
-            self.hide_error_single()
-            self.label_student_grade_converted.setText(f"Student's Grade: {self.grade_conversion(highest_grade, student_grade)}")
-            self.display_success_single()
-    
-    def calculate_multiple(self):
-        """
-        Function to calculate multiple students curved grades and write results to grades.csv
-        """
-        try:
-            total_students = int(self.input_total_students.text())
-            grades = [int(grade) for grade in self.input_student_grades.text().split()]
-            while len(grades) > total_students:
-                grades.pop()
-            best_grade = int(max(grades))
-        except:
-            self.display_error_multiple()
-        else:
-            self.hide_error_multiple()
-            with open('grades.csv', 'w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(["student", "grade", "curved_grade"])
-                for student, grade in enumerate(grades):
-                    csv_writer.writerow(["student "+str(student+1), grade, self.grade_conversion(best_grade, grade)])
-            self.display_success_multiple()
-        
     def display_success_multiple(self):
         """
         Function to reveal success message for multiple student calculation
         """
+        self.label_saved.setStyleSheet("color: rgb(75, 181, 67);")
         self.label_saved.setHidden(False)
     
     def hide_success_multiple(self):
@@ -97,41 +62,6 @@ class Controller(QMainWindow, Ui_MainWindow):
         """
         self.label_warning_multiple_1.setHidden(True)
         self.label_warning_multiple_2.setHidden(True)
-
-    def display_success_single(self):
-        """
-        Function to display converted numerical grade for single student calculation
-        """
-        self.label_student_grade_converted.setHidden(False)
-    
-    def hide_success_single(self):
-        """
-        Function to hide converted numerical grade for single student calculation
-        """
-        self.label_student_grade_converted.setHidden(True)
-
-    def display_error_single(self):
-        """
-        Function to display error message for single student calculation
-        """
-        self.label_warning_single_1.setHidden(False)
-        self.label_warning_single_2.setHidden(False)
-
-    def hide_error_single(self):
-        """
-        Function to hide error message for single student calculation
-        """
-        self.label_warning_single_1.setHidden(True)
-        self.label_warning_single_2.setHidden(True)
-
-    def clear_single_inputs(self):
-        """
-        Function to clear input data from single student calculation
-        """
-        self.hide_error_single()
-        self.hide_success_single()
-        self.input_highest_grade.setText("")
-        self.input_student_grade.setText("")
     
     def clear_multiple_inputs(self):
         """
@@ -139,8 +69,7 @@ class Controller(QMainWindow, Ui_MainWindow):
         """
         self.hide_error_multiple()
         self.hide_success_multiple()
-        self.input_total_students.setText("")
-        self.input_student_grades.setText("")
+        self.input_csv_path.setText("")
     
     def grade_conversion(self, highest_grade, grade):
         """
@@ -159,3 +88,49 @@ class Controller(QMainWindow, Ui_MainWindow):
             return "D"
         else:
             return "F"
+
+    # def get_output_file_name(self):
+    #     output_file_name = input("Output file name: ").strip()
+        
+    #     while os.path.isfile(f'files/{output_file_name}'):
+    #         decision = input("Overwrite existing file (y/n): ").strip().lower()
+    #         while decision != 'y' and decision != 'n':
+    #             decision = input("Enter (y/n): ").strip().lower()
+
+    #         if decision == 'y':
+    #             return output_file_name
+
+    #         if decision == 'n':
+    #             output_file_name = input("New output file name: ").strip()
+
+    #     return output_file_name
+
+    def parse_csv(self, file):
+        """
+        Function to parse input csv and return highest grade found and each line in CSV file located in same location as main.py
+        :param file: opened csv file
+        :return: float, list
+        """
+        highest_grade = 0.
+        data = []
+        for line in file:
+            data.append(line.strip())
+            line = line.strip().split(',')
+            current_grade = float(line[1])
+            if current_grade  > highest_grade:
+                highest_grade = float(line[1])
+        file.close()
+        return highest_grade, data
+
+    def output_csv(self, highest_grade, data):
+        
+        with open(f'curved_grades.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Student', 'Grade', 'Curved Grade'])
+            for line in data:
+                line = line.strip().split(',')
+                student = line[0]
+                grade = float(line[1])
+                csv_writer.writerow([student, grade, self.grade_conversion(highest_grade, grade)])
+        self.clear_multiple_inputs()
+        self.display_success_multiple()
